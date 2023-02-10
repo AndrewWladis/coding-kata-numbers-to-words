16d09494f8602c86219f2d0e57f4c76ce6c6ffdc
AndrewWladis <andrewwladis@gmail.com>
2023-02-09 23:39:41 -0500

    added reverse

--- index.js
+++ index.js
@@ -2,6 +2,33 @@ const ones = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'n
 const teens = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eightteen', 'nineteen'];
 const tens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
+function wordToNum(word) {
+    let returnNum = 0;
+    let newWord = word.split(' ').reverse();
+    let length = newWord.length;
+    let num = 3;
+
+    for (let i = 0; i < length; i++) {
+        if (teens.includes(newWord[i])) {
+            num = 2;
+            returnNum += teens.indexOf(newWord[i]) + 11;
+        } else {
+            if (newWord[i].includes('-thousand')) {
+                returnNum += (ones.indexOf(newWord[i].replace(/-thousand/g, '')) + 1) * 1000;
+            } else if (newWord[i].includes('-hundred')) {
+                returnNum += (ones.indexOf(newWord[i].replace(/-hundred/g, '')) + 1) * 100;
+            } else if (tens.includes(newWord[i])) {
+                returnNum += (tens.indexOf(newWord[i]) + 1) * 10;
+                console.log('this is being called')
+            } else {
+                returnNum += ones.indexOf(newWord[i]) + 1;
+            }
+        }
+    }
+
+    return returnNum;
+}
+
 function numToWord(num) {
     let resultArr = [];
     let arr = num.toString().split('').reverse().map((item) => Number(item))
@@ -17,15 +44,15 @@ function numToWord(num) {
             resultArr.push(tens[arr[1] - 1])
         } else if (arr[1] === 1 && arr[0] != 0) {
             resultArr.push(teens[arr[0] - 1])
+        } else {
+            resultArr.push(tens[arr[1] - 1])
         }
     }
     if (length >= 3) {
-        if (arr[0] === 0 && arr[1] === 0 && arr[2] != 0) {
-            resultArr.push(ones[arr[2] - 1] + '-hundred')
-        } if (arr[2] === 0) {
+        if (arr[2] === 0) {
 
         } else {
-            resultArr.push(ones[arr[2] - 1] + '-hundred and')
+            resultArr.push(ones[arr[2] - 1] + '-hundred')
         }
     }
     if (length === 4) {
@@ -34,5 +61,8 @@ function numToWord(num) {
     return resultArr.reverse().join(' ');
 }
 
-//change num to change value
+//change paramter number to whatever value you want
 console.log(numToWord(2319));
+
+//this reverses the function 🔄
+wordToNum(numToWord(2319))
